682265137ad0eee1705dd8b18f8dde7db9d1bb6e
Mayank Kashyap <anshumank72@gmail.com>
2023-10-18 13:43:47 +0530

    first commit

--- src/component/FrontPage/FrontPage.jsx
+++ src/component/FrontPage/FrontPage.jsx
@@ -1,5 +1,4 @@
 import React, { Component } from "react";
-import style from "./FrontPage.module.css";
 import Header from "./FronPageComponents/FrontHeader/FrontHeader";
 import MainBody from "./FronPageComponents/MainBody/MainBody";
 
